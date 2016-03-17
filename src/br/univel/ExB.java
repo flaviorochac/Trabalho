26930205897217ec85ec85ed93e8b09badb88b81
Flávio Rocha Cezarino <ghostreapedrc@gmail.com>
2016-03-17 18:43:53 -0300

    Iniciado Segunda forma geometrica

---
+++ src/br/univel/ExB.java
@@ -0,0 +1,26 @@
+package br.univel;
+
+//Trabalho Primeira Forma.
+
+public class ExB implements Desenho {
+	
+	public static int ALT = 8;
+
+	@Override
+	public void desenhar() {
+		
+		System.out.print("a)\n\n");
+		
+		int n = 1;
+		for (int l = 0;l < ALT; l++) {
+			for (int c = 0; c < n; c++){
+				System.out.print("#");
+				
+			}
+			n++;
+			System.out.print("\n");
+		}
+		
+	}
+	
+}
