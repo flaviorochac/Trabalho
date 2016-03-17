ef6846099b7bf508e31721ad80b2d29c725997cb
Flávio Rocha Cezarino <ghostreapedrc@gmail.com>
2016-03-17 18:27:13 -0300

    Desenvolvendo os Clacesses Pricipais, A lista na clace principal e a Interface para executar o metodo do desenho.

---
+++ src/br/univel/Desenho.java
@@ -0,0 +1,9 @@
+package br.univel;
+
+// Trabalho.
+
+public interface Desenho {
+	
+	public void desenhar();
+
+}
--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -1,11 +1,17 @@
 package br.univel;
 
-/* Trabalho para moldar formas geometricas
- * 
- * 
- * 
- * 
- */
-public class Principal {
+// Trabalho para moldar formas geometricas
 
+import java.util.LinkedList;
+import java.util.List;
+
+public class Principal {
+	
+	public static void main(String [] args){
+		List<Desenho> lista = new LinkedList<>();
+		
+		for (Desenho d : lista){
+			d.desenhar();
+		}
+	}	
 }
